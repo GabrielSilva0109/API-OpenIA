8646f5173f246ba9055757d9c444591c2a385641
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-04-10 09:19:37 -0300

    Update

--- server/src/controller/prompt-controller.js
+++ server/src/controller/prompt-controller.js
@@ -1,15 +1,26 @@
 const openai = require("../config/openai")
+const inputPrompt = require("../models/input-prompt")
+
 
 module.exports = {
     async sendText(req, res){
         const openaiAPI =openai.configuration()
-        
+        const inputModel = new inputPrompt(req.body)
         try {
             const response = await openaiAPI.createCompletion(
-                openia.textCompletion("Me de nome de artigos de node")
+                openia.textCompletion(inputModel)
             )
+
+            return res.status(200).json({
+                sucess: true,
+                data: response.data.choices[0].text
+            })
         } catch (error) {
-            
+            return res.status(400).json({
+                sucess: false,
+                error: error.response ? error.response : 
+                "There was an inssue on the server"
+            })
         }
     }
 }
