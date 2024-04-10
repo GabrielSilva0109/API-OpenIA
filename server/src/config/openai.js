8f723cbfe724bf2fba5b3d40c2bf13ed06727a48
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-04-10 09:06:16 -0300

    Update openai.js

--- server/src/config/openai.js
+++ server/src/config/openai.js
@@ -5,18 +5,19 @@ module.exports = class openai{
         const configuration = new Configuration({
             apyKey: process.env.OPENAI_API_KEY
         })
+
+        return new OpenAIApi(configuration)
+    }
+
+    static textCompletion({prompt}){
+        return{
+            model: "text-davinci-300",
+            prompt: `${prompt}`,
+            temperature: 0,
+            max_tokens: 3500,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0
+        }
     }
 }
-
-
-const openai = new OpenAIApi(configuration)
-
-const response = await openai.createCompleton({
-    model: "text-davinci-300",
-    prompt: "diga um nome?\n\nGabriel",
-    temperature: 0,
-    max_tokens: 256,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0
-})
